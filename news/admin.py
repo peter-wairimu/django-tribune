8671b0caee7eb54427ce80a77fde219439aa9b63
peter-wairimu <peter.wairimu@student.moringaschool.com>
2021-10-06 11:39:18 +0300

    initial commit

---
+++ news/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Editor,Article,Tags
+
+class ArticleAdmin(admin.ModelAdmin):
+    filter_horizontal= ('tags',)
+
+# Register your models here.
+admin.site.register(Editor)
+admin.site.register(Article,ArticleAdmin)
+admin.site.register(Tags)
